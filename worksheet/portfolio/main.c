f3f25d8d1e3d6bb855f4ec916463198f06e2265f
munnuspider <bhavya.trivedi2007@gmail.com>
2026-03-23 22:37:12 +0000

    yes

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -5,9 +5,7 @@
 #include "shapes.h"
 
 int main( void ) {
-
-    Point p1 = makePoint(3.0, 4.0);
-    printf("Point created at: (%.1f, %.1f)\n", p1.x, p1.y)
+    
     // test your code by calling the functions and printing the output 
     return 0;
 }
--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -12,6 +12,21 @@ Point makePoint( float x, float y ) {
     return new;
 }
 
+Line makeLine(Point p1, Point p2) {
+    Line l;
+    l.p[0] = p1;
+    l.p[1] = p2;
+    return l;
+}
+
+Triangle makeTriangle(Point p1, Point p2, Point p3) {
+    Triangle t;
+    t.p[0] = p1;
+    t.p[1] = p2;
+    t.p[2] = p3;
+    return t;
+}
+
 // complete other functions below
 // - start with stubs as above
 // - compile regularly to test syntax
